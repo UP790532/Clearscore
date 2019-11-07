52acb893cd3b6b3a620bc3bc208e3a2e6ccb2873
Jack <Jacksherwood189@gmail.com>
2019-11-07 21:35:12 +0000

    Initial Commit

---
+++ ClearScoreTechnicalTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearScoreTechnicalTask
+//
+//  Created by Jack Sherwood on 07/11/2019.
+//  Copyright © 2019 Jack Sherwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
